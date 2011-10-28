edef500657ddafcc32f78548282bd7537b8fb908
Dashiell Gough <dashiellgough@DG.local>
2011-10-28 06:03:36 -0700

    Initial Commit

---
+++ softbodytest/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  softbodytest
+//
+//  Created by Dashiell Gough on 10/28/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ softbodytest/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  softbodytest
+//
+//  Created by Dashiell Gough on 10/28/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
